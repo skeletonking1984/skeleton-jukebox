230940069b6660852f315a5c877370321b91a332
skeletonking1984 <techgazetteteam@gmail.com>
2026-03-17 14:28:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,56 +14,13 @@ app.use(express.static(process.cwd()));
 
 // 🔥 CLEAN RANDOM LIST (all these play reliably)
 const fallbackSongs = [
-  { id: "kXYiU_JCYtU", title: "Linkin Park - In The End (Official Music Video)" },
+  { id: "kXYiU_JCYtU", title: "Linkin Park - Numb (Official Music Video)" },
   { id: "hTWKbfoikeg", title: "Nirvana - Smells Like Teen Spirit (Official Music Video)" },
-  { id: "CD_8iYQh3lY", title: "Metallica - Enter Sandman (Official Music Video)" },
-  { id: "v2AC41dglnM", title: "AC/DC - Thunderstruck (Official Video)" },
-  { id: "-tJYN-eG1zk", title: "Queen - We Will Rock You (Official Video)" },
-  { id: "z5rRZdiu1zI", title: "Beastie Boys - Sabotage (Official Music Video)" },
-  { id: "b8-tXG8KrWs", title: "Rage Against The Machine - Killing In The Name (Official Video)" },
-  { id: "6mYxQ6s3dF4", title: "Foo Fighters - Everlong (Official HD Video)" },
-  { id: "dQw4w9wgxcq", title: "Rick Astley - Never Gonna Give You Up (Official Music Video)" },
-  { id: "L_jWHffIx5E", title: "Smash Mouth - All Star (Official Music Video)" },
-  { id: "1w7OgIMMRc4", title: "Guns N' Roses - Sweet Child O' Mine (Official Music Video)" },
-  { id: "fJ9rUzIMcZQ", title: "Queen - Bohemian Rhapsody (Official Video Remastered)" }, // Fixed: correct ID
-  { id: "04mfKJWDSzI", title: "AC/DC - Back In Black (Official 4K Video)" },
-  { id: "GQMlWwIXg3M", title: "Guns N' Roses - Welcome To The Jungle (Official Music Video)" },
-  { id: "5Wp4dYFGpKw", title: "Guns N' Roses - November Rain (Official Music Video)" },
-  { id: "bESGLojNYSo", title: "The Cranberries - Zombie (Official Music Video)" },
-  { id: "3YxaaGgTQYM", title: "Radiohead - Creep (Official Video)" },
-  { id: "ungq4OqM3So", title: "Oasis - Wonderwall (Official Video)" },
-  { id: "y6120QOlsfU", title: "Green Day - Basket Case (Official Music Video)" },
-  { id: "hEMm7gxBYSc", title: "System Of A Down - Chop Suey! (Official HD Video)" },
-  { id: "CSvFpBOe8eY", title: "Linkin Park - Numb (Official Music Video)" },
-  { id: "M7lc1UVf-VE", title: "Michael Jackson - Billie Jean (Official Video)" },
-  { id: "fJ9rUzIMcZQ", title: "Eurythmics - Sweet Dreams (Are Made Of This) (Official Video)" }, // Note: same ID as Queen? No, wait—actually different; corrected if needed but using known good
-  { id: "1lyu1KKwC74", title: "The White Stripes - Seven Nation Army (Official Music Video)" },
-  { id: "0J2QdDbelmY", title: "a-ha - Take On Me (Official Music Video)" },
-  { id: "djV11Xbc914", title: "Evanescence - Bring Me To Life (Official Music Video)" },
-  { id: "8sgycukafqQ", title: "Red Hot Chili Peppers - Californication (Official Music Video)" },
-  { id: "YlUKcNNmywk", title: "Red Hot Chili Peppers - Under The Bridge (Official Music Video)" },
-  { id: "KxDq5KX3D4Y", title: "Soundgarden - Black Hole Sun (Official Music Video)" },
-  { id: "PBHR-In4E0Q", title: "Foo Fighters - My Hero (Official HD Video)" },
-  { id: "4fWyzwo1xg0", title: "Black Sabbath - Paranoid (Official Video)" },
-  { id: "5sNWbJghqCA", title: "Black Sabbath - Iron Man (Official Video)" },
-  { id: "l482Tsmfqp8", title: "AC/DC - T.N.T. (Official Video)" },
-  { id: "pAgnJDJN4VA", title: "Blur - Song 2 (Official Music Video)" },
-  { id: "E0E0ynyIUsg", title: "Stone Temple Pilots - Interstate Love Song (Official Music Video)" },
-  { id: "xGytDsqkQY8", title: "Stone Temple Pilots - Plush (Official Music Video)" },
-  { id: "4N0R0W3N9fM", title: "The Smashing Pumpkins - 1979 (Official Music Video)" },
-  { id: "NOG3eus4ZSo", title: "The Smashing Pumpkins - Bullet With Butterfly Wings (Official Music Video)" },
-  { id: "6Ejga4kJUts", title: "The Smashing Pumpkins - Today (Official Music Video)" },
-  { id: "6Fu5PuquH4E", title: "Rammstein - Du Hast (Official Video)" },
-  { id: "QRg_8NNPTD8", title: "Limp Bizkit - Rollin' (Official Music Video)" },
-  { id: "MEbJWCnACn8", title: "Limp Bizkit - Break Stuff (Official Music Video)" },
-  { id: "EDlC7oG_2W4", title: "Limp Bizkit - Nookie (Official Music Video)" },
-  // Added a few more solid verified ones to hit exactly 50 (replacing the bad ones)
-  { id: "etAIpkdhU9Q", title: "AC/DC - Hells Bells (Official 4K Video)" },
-  { id: "BciSnt4q3iE", title: "Metallica - Nothing Else Matters (Official Music Video)" },
-  { id: "eVTXPUF4Oz4", title: "Linkin Park - In The End (Official HD Music Video)" }, // Studio version alt
-  { id: "NWU33fvPxd0", title: "Foo Fighters - Everlong (Live At Wembley Stadium, 2008)" },
-  { id: "6vImyP5EYc8", title: "AC/DC - Back In Black (Live at Donington, 8/17/91)" },
-  { id: "vabnZ9-ex7o", title: "Nirvana - Come As You Are (Official Music Video)" } // Kept but correct title now
+  { id: "XZuM4zFg-60", title: "Metallica - Enter Sandman (Official Music Video)" },
+  { id: "XaiYxczjZ0U", title: "Godsmack - Voodoo" },
+  { id: "H-iPavAXQUk", title: "Kavinsky - Nightcall" },
+  { id: "xGytDsqkQY8", title: "Semisonic - Closing Time (Official Music Video)" },
+
 ];
 
 function getRandomSong() {
